45c1b4f64815cc74f321d8227c1b995a97c801a1
cromeoli <cromero@iesrafaelalberti.es>
2022-12-09 21:08:37 +0100

    Travesía del overlay semicompleta

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,22 +1,61 @@
 let burgerButton = document.getElementById("burger")
+let logInButton = document.getElementById("logInButton")
+let signUpButton = document.getElementById("signUpButton")
 let overlayCloseArea = document.querySelector('.overlayCloseArea')
 
 
-function openOverlay(){
-    let overlayMenu = document.querySelector('.overlay')
+function openHome(){
+    let homeOverlay = document.querySelector('.homeOverlay')
+    let overlayClose = document.querySelector('.overlayCloseArea')
+
+    homeOverlay.style.display = "flex"
+    overlayClose.style.display = "flex"
+}
+
+function openGuest(){
+    let guestOverlay = document.querySelector('.guestOverlay')
     let overlayArea = document.querySelector('.overlayCloseArea')
 
-    overlayArea.style.display = "flex"
-    overlayMenu.style.display = "flex"
+    guestOverlay.style.display = 'flex'
+    overlayArea.style.display = 'flex'
 }
+
+function openRegister(){
+    let registerOverlay = document.querySelector('.registerOverlay')
+    let overlayArea = document.querySelector('.overlayCloseArea')
+    let guestOverlay = document.querySelector('.guestOverlay')
+
+    registerOverlay.style.display = 'flex'
+    overlayArea.style.display = 'flex'
+    guestOverlay.style.display = 'none'
+}
+
+function openLogin(){
+    let loginOverlay = document.querySelector('.loginOverlay')
+    let overlayArea = document.querySelector('.overlayCloseArea')
+    let guestOverlay = document.querySelector('.guestOverlay')
+
+    loginOverlay.style.display = 'flex'
+    overlayArea.style.display = 'flex'
+    guestOverlay.style.display = 'none'
+}
+
 
 function closeOverlay(){
-    let overlayMenu = document.querySelector('.overlay')
-    let overlayArea = document.querySelector('.overlayCloseArea')
+    let overlayClose = document.querySelector('.overlayCloseArea')
+    let guestOverlay = document.querySelector('.guestOverlay')
+    let registerOverlay = document.querySelector('.registerOverlay')
+    let loginOverlay = document.querySelector('.loginOverlay')
+    let menuOverlay = document.querySelector('.homeOverlay')
 
-    overlayArea.style.display = "none"
-    overlayMenu.style.display = "none"
+    overlayClose.style.display = "none"
+    menuOverlay.style.display = "none"
+    guestOverlay.style.display = "none"
+    registerOverlay.style.display = "none"
+    loginOverlay.style.display = "none"
 }
 
-burgerButton.addEventListener("click", openOverlay)
-overlayCloseArea.addEventListener("click",closeOverlay)
+overlayCloseArea.addEventListener("click", closeOverlay)
+burgerButton.addEventListener("click", openGuest)
+logInButton.addEventListener('click', openLogin)
+signUpButton.addEventListener('click', openRegister)
