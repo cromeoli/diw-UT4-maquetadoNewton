3ead9bc72de02871aeb82103724d2480487fd649
Christian <cromoli1808@g.educaand.es>
2022-12-11 21:07:47 +0100

    Añadida hoja de estilos para modo claro de la página principal

---
+++ src/assets/js/lightTheme.js
@@ -0,0 +1,35 @@
+
+let logoNewton = document.getElementById("")
+let inputbar1 = document.getElementById("inputBar1")
+let inputbar2 = document.getElementById("inputBar2")
+
+let k1 = document.getElementById("k1")
+let k2 = document.getElementById("k2")
+let k3 = document.getElementById("k3")
+let k4 = document.getElementById("k4")
+let k5 = document.getElementById("k5")
+let k6 = document.getElementById("k6")
+let k7 = document.getElementById("k7")
+let k8 = document.getElementById("k8")
+let k9 = document.getElementById("k9")
+let k10 = document.getElementById("k10")
+let k11 = document.getElementById("k11")
+let k12 = document.getElementById("k12")
+let k13 = document.getElementById("k13")
+let k14 = document.getElementById("k14")
+let k15 = document.getElementById("k15")
+let k16 = document.getElementById("k16")
+
+let resultArea = document.getElementById('backgroundResultArea')
+let mainBackground = document.querySelector("body")
+
+checkbox.addEventListener('change', function (){
+    if (this.checked) {
+        mainBackground.classList.add('')
+
+
+    } else {
+        console.log("Checkbox is not checked..");
+
+    }
+})
